9a22deabc6f349066029a07991c538efa67a83e0
Vinicius Padovan <vini.vptds@gmail.com>
2025-08-19 10:12:57 -0300

    Update server.js and demo.html file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.static(path.join(__dirname, "assets")));
 
 // Default route
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "routes", "demo.html"));
+    res.sendFile(path.join(__dirname, "public", "demo.html"));
 });
 
 const routes = [
